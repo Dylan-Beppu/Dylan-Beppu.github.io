3763fd0b764863842cefc99a3168bc0587c461ee
Dylan Beppu <dbeppu.hi@gmail.com>
2025-11-13 15:33:33 -0800

    Merged into index

--- projectLoader.js
+++ projectLoader.js
@@ -1,37 +1,38 @@
+// Old one
+// function generateHTML(projects) {
+//     return projects.map(project => `
+//         <div class="Project">
+//                 <h2>${project.name}</h2>
+//                 <div class="Project-content">
+//                     <img class="Project-img" src="${project.image}" alt="${project.name} Image" />
+//                     <div class="Project-overlay">
+//                         <p>${project.description}</p>
+//                         <a href="${project.link}" class="btnInline">
+//                             ${project.sourceType === 'github' ? '<i class="fab fa-brands fa-github"></i>' : '<img class="inlineRblx" src="assets/img/rblxIcon.png"/>'}
+//                             Find it on ${project.sourceType}
+//                         </a>
+//                     </div>
+//                 </div>
+//             </div>
+//     `).join('');
+// }
+
+
+
 function generateHTML(projects) {
     return projects.map(project => `
-        <div class="Project">
-                <h2>${project.name}</h2>
-                <div class="Project-content">
-                    <img class="Project-img" src="${project.image}" alt="${project.name} Image" />
-                    <div class="Project-overlay">
-                        <p>${project.description}</p>
-                        <a href="${project.link}" class="btnInline">
-                            ${project.sourceType === 'github' ? '<i class="fab fa-brands fa-github"></i>' : '<img class="inlineRblx" src="assets/img/rblxIcon.png"/>'}
-                            Find it on ${project.sourceType}
-                        </a>
-                    </div>
-                </div>
-            </div>
+		<div class="NeoProject" id=${project.name} style="background-image: url('${project.image}');">	
+			<a href="${project.link}" target="_blank" style="color: inherit; text-decoration: none;">
+				<div class="NeoProjectOverlay">
+					<h2>${project.name}</h2>
+					<p>${project.description}</p>
+				</div>
+			</a>
+		</div>
     `).join('');
 }
 
 
-
-  
-//   <div class="obj-left">
-//   <div class="img-box">
-//     <img class="left-img" src="${project.image}" alt="..." />
-//   </div>
-//   <div class="right-txt">
-//     <h3>${project.name}</h3>
-//     <a href="${project.link}" class="btnInline">
-//       ${project.sourceType === 'github' ? '<i class="fab fa-brands fa-github"></i>' : '<img class="inlineRblx" src="assets/img/rblxIcon.png"/>'}
-//       Find it on ${project.sourceType}
-//     </a>
-//     <p>${project.description}</p>
-//   </div>
-// </div>
 
 document.addEventListener('DOMContentLoaded', () => {
     fetch('assets/projects.json')
